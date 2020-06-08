32c90b2470914831650abdad2b7e81f224d67ef5
Henrik Forsberg <henrikforsberg.dev@gmail.com>
2020-06-08 23:11:55 +0200

    Day 55 - Set up Project 14

---
+++ Project14/Project14/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by Henrik Forsberg on 2020-06-08.
+//  Copyright © 2020 Henrik Forsberg. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
